1ff0715bb1af36b3a9b1a3d032495f82ad9bb941
Lee Tae Hoon <98hoon@gmail.com>
2018-01-16 16:06:01 +0900

    quiz1

---
+++ quiz/Quiz1.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+// 주어진 array에서 3의 배수 찾기
+import UIKit
+
+var str = "Hello, playground"
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+var retArray1 : [Int] = []
+for item in array {
+    if item%3 == 0 {
+        retArray1.append(item)
+    }
+}
